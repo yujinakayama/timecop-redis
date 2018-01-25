780c29f5a3103bf0bb7b12bea0de47c63a753019
Yuji Nakayama <nkymyj@gmail.com>
2018-01-25 18:03:30 +0900

    Bump version to 0.2.0

--- lib/timecop/redis/version.rb
+++ lib/timecop/redis/version.rb
@@ -3,8 +3,8 @@ class Timecop
     # http://semver.org/
     module Version
       MAJOR = 0
-      MINOR = 1
-      PATCH = 1
+      MINOR = 2
+      PATCH = 0
 
       def self.to_s
         [MAJOR, MINOR, PATCH].join('.')
